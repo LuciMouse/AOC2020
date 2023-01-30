1b050c4e263e3a075b828d19137d7c9aab64a64a
Your Name <you@example.com>
2023-01-29 23:24:11 -0800

    day8: part 2 passed

--- Day8.py
+++ Day8.py
@@ -8,19 +8,19 @@ def is_visible(grid_df, curr_location):
     :param location: location to check
     :return: bool of whether the location is visible from outside the grid
     """
-    x_coord = curr_location[0]
-    y_coord = curr_location[1]
+    row_coord = curr_location[0]
+    col_coord = curr_location[1]
 
-    location_value = grid_df.iloc[x_coord,y_coord]
+    location_value = grid_df.iloc[row_coord,col_coord]
     #check each side
     cardinal_series_ls = [
-        grid_df.loc[x_coord, :y_coord - 1], #west of curr_position
-        grid_df.loc[:x_coord-1, y_coord],  # north of curr_position
-        grid_df.loc[x_coord, y_coord+1:],  # east of curr_position
-        grid_df.loc[x_coord+1:, y_coord],  # south of curr_position
+        grid_df.loc[row_coord, :col_coord - 1], #west of curr_position
+        grid_df.loc[:row_coord-1, col_coord],  # north of curr_position
+        grid_df.loc[row_coord, col_coord+1:],  # east of curr_position
+        grid_df.loc[row_coord+1:, col_coord],  # south of curr_position
     ]
-    for curr_position in cardinal_series_ls:
-        if curr_position.max() < location_value: #if the maximum value is less than the tree in curr_location
+    for curr_direction_s in cardinal_series_ls:
+        if curr_direction_s.max() < location_value: #if the maximum value is less than the tree in curr_location
             return True
     return False
 
@@ -50,7 +50,40 @@ def calculate_scenic_score(grid_df,curr_location):
     :param curr_location: location to check
     :return: tree score of current location
     """
+    row_coord = curr_location[0]
+    col_coord = curr_location[1]
 
+    location_value = grid_df.iloc[row_coord, col_coord]
+    # check each side, front of list is closest to tree, empty series if edge tree
+    cardinal_series_ls = [
+        grid_df.loc[row_coord, :col_coord - 1][::-1] if col_coord>0 else pd.Series(),  # west of curr_position
+        grid_df.loc[:row_coord - 1, col_coord][::-1] if row_coord>0 else pd.Series(),  # north of curr_position
+        grid_df.loc[row_coord, col_coord + 1:] if col_coord<grid_df.index.size else pd.Series(),  # east of curr_position
+        grid_df.loc[row_coord + 1:, col_coord] if row_coord<grid_df.columns.size else pd.Series(),  # south of curr_position
+    ]
+    product =1
+    for curr_direction_s in cardinal_series_ls:
+        taller_trees_bool_s = curr_direction_s.apply(lambda x: x>=location_value).reset_index(drop= True) #reset the index
+        taller_trees_s = taller_trees_bool_s[taller_trees_bool_s]
+        if taller_trees_s.empty:#all trees are shorter than current tree
+            product*=len(curr_direction_s)
+        else:
+            closest_tree_index = taller_trees_s.index.values[0]
+            product*=closest_tree_index+1
+    return product
+
+def max_scenic_score(raw_data):
+    """
+    calculates scenic score from each position and returns the highest one
+    :param raw_data: raw_input
+    :return: highest scenic_score
+    """
+    split_data = [y for y in raw_data.split("\n")]
+    grid_df = pd.DataFrame([[*x] for x in split_data]).applymap(lambda x: int(x))
+    coord_list = [(x,y) for x in range(1,grid_df.index.size-1) for y in range(1,grid_df.columns.size-1)] #it's caught in the code, but since all edge trees have one 0 view, don't bother to test them
+    scenic_score_ls = list(map(lambda x: calculate_scenic_score(grid_df,x),coord_list))
+    return max(scenic_score_ls)
 
 if __name__=="__main__":
     print(f"there are {num_visible(data)} trees visible from outside the grid")
+    print(f"the highest scenic score possible is {max_scenic_score(data)}")
--- test_Day8.py
+++ test_Day8.py
@@ -8,7 +8,7 @@ class test_is_visible(unittest.TestCase):
         with open("Day8_test_input.txt") as input_file:
             split_data = [y for y in input_file.read().split("\n")]
 
-            grid_df = pd.DataFrame([[*x] for x in split_data]).applymap(lambda x: int(x))
+        grid_df = pd.DataFrame([[*x] for x in split_data]).applymap(lambda x: int(x))
         coord_list = [(x,y) for x in range(1,grid_df.index.size-1) for y in range(1,grid_df.columns.size-1)]
         self.assertEqual(
             list(map(lambda x: Day8.is_visible(grid_df,x),coord_list)),
@@ -24,6 +24,37 @@ class test_num_visible(unittest.TestCase):
             21
         )
 
+class test_calculate_scenic_score(unittest.TestCase):
+    def test_calculate_scenic_score_1(self):
+        with open("Day8_test_input.txt") as input_file:
+            split_data = [y for y in input_file.read().split("\n")]
 
+        grid_df = pd.DataFrame([[*x] for x in split_data]).applymap(lambda x: int(x))
+        self.assertEqual(
+            [
+                Day8.calculate_scenic_score(grid_df,(1,2)),
+                Day8.calculate_scenic_score(grid_df,(3,2)),
+                Day8.calculate_scenic_score(grid_df, (0, 2)),
+                Day8.calculate_scenic_score(grid_df, (1, 4)),
+                Day8.calculate_scenic_score(grid_df, (4, 2)),
+                Day8.calculate_scenic_score(grid_df, (1, 0)),
+            ],
+            [
+                4,
+                8,
+                0,
+                0,
+                0,
+                0,
+            ]
+        )
+class test_max_scenic_score(unittest.TestCase):
+    def test_max_scenic_score_1(self):
+        with open("Day8_test_input.txt") as input_file:
+            raw_data = input_file.read()
+        self.assertEqual(
+            Day8.max_scenic_score(raw_data),
+            8
+        )
 if __name__ == '__main__':
     unittest.main()
