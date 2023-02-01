5877e5dba97567cf109d897b333739e7cb95ffb6
Your Name <you@example.com>
2023-01-31 17:58:16 -0800

    Finish day 11 part1

---
+++ Day11.py
@@ -0,0 +1,76 @@
+from aocd import data
+
+
+class monkey:
+    def __init__(self, monkey_number, starting_items, operation, test_tuple=None):
+        self.monkey_number = monkey_number
+        self.starting_items = starting_items
+        self.operation = operation
+        self.test = lambda value: test_tuple[1] if value % test_tuple[0]==0 else test_tuple[2]
+
+
+def make_monkey(unformatted_data):
+    """
+    given a block of unformatted data, makes a monkey object
+    :param unformatted_data: unformatted strings
+    :return: monkey object
+    """
+    unformatted_data_ls = unformatted_data.split("\n")
+
+    monkey_number = int(unformatted_data_ls[0].split(' ')[1][:-1])
+    starting_items = [int(x) for x in unformatted_data_ls[1].split(': ')[1].split(", ")]
+    operation = unformatted_data_ls[2].split('= ')[1]
+    test_tuple = (
+        int(unformatted_data_ls[3].split(' ')[-1]),
+        int(unformatted_data_ls[4][-1]),
+        int(unformatted_data_ls[5][-1])
+    )
+    new_monkey = monkey(monkey_number, starting_items, operation, test_tuple)
+    return new_monkey
+
+def monkey_turn(monkey_index,monkey_ls):
+    """
+    describes a single monkey's turn
+    :param monkey_index: index of monkey that is inspecting and throwing
+    :param monkey_ls: list of all monkey objects
+    :return: updated monkey_ls
+    """
+    curr_monkey = monkey_ls[monkey_index]
+    for curr_item in curr_monkey.starting_items:
+        old = curr_item
+        inspection_worry_level = eval(curr_monkey.operation)
+        post_inspection_level = inspection_worry_level//3
+        recipient_monkey = curr_monkey.test(post_inspection_level)
+        monkey_ls[recipient_monkey].starting_items.append(post_inspection_level)
+    curr_monkey.starting_items = []
+    return monkey_ls
+
+def round_implementer(monkey_ls, item_count_ls):
+    """
+    describes a round of monkey shenanigans
+    :param monkey_ls: list of monkey objects
+    :return: updated monkey list
+    """
+    for curr_index in range(len(monkey_ls)):
+        item_count_ls[curr_index]+=len(monkey_ls[curr_index].starting_items)
+        monkey_ls = monkey_turn(curr_index,monkey_ls)
+    return monkey_ls, item_count_ls
+
+def calculate_monkey_business(raw_input, num_rounds):
+    # calculates the level of monkey business over num_rounds of shenanigans
+    monkey_ls = []
+    for unformatted_curr_monkey in raw_input.split('\n\n'):
+        new_monkey = make_monkey(unformatted_curr_monkey)
+        monkey_ls.append(new_monkey)
+
+    item_count_ls = [0 for x in monkey_ls]
+    for round_num in range(num_rounds):
+        monkey_ls, item_count_ls = round_implementer(monkey_ls, item_count_ls)
+    most_active_monkeys_ls = item_count_ls.copy()
+    most_active_monkeys_ls.sort(reverse=True)
+    return (most_active_monkeys_ls[0]*most_active_monkeys_ls[1])
+
+
+if __name__ == '__main__':
+    print(f"level of monkey business: {calculate_monkey_business(data,20)}")
+
---
+++ test_Day11.py
@@ -0,0 +1,138 @@
+import unittest
+import Day11
+
+
+class TestMonkey(unittest.TestCase):
+    def test_new_monkey(self):
+        monkey_0 = Day11.monkey(
+            0,
+            [79, 98],
+            "old * 19",
+            (23, 1, 3)
+        )
+
+        monkey_2 = Day11.monkey(
+            2,
+            [79, 60, 97],
+            "old * old",
+
+        )
+        monkey_3 = Day11.monkey(
+            3,
+            [79, 98],
+            "old + 3",
+
+        )
+        old = 3
+
+        self.assertEqual(
+            [monkey_0.monkey_number, monkey_0.starting_items, eval(monkey_0.operation), monkey_0.test(11)],
+            [0, [79, 98], 57, 3])
+
+
+class TestMakeMonkey(unittest.TestCase):
+    def test_make_monkey(self):
+        monkey_0 = Day11.monkey(
+            0,
+            [79, 98],
+            "old * 19",
+            (23, 2, 3)
+        )
+        test_monkey = Day11.make_monkey(
+            "Monkey 0:\nStarting items: 79, 98\nOperation: new = old * 19\nTest: divisible by 23\nIf true: throw to monkey 2\nIf false: throw to monkey 3"
+        )
+        self.assertEqual(
+            [monkey_0.monkey_number, monkey_0.starting_items, monkey_0.operation, monkey_0.test(11)],
+            [test_monkey.monkey_number, test_monkey.starting_items, test_monkey.operation, test_monkey.test(11)]
+
+        )
+class TestMonkeyTurn(unittest.TestCase):
+    def test_monkey_turn(self):
+        monkey_ls = [
+            Day11.monkey(
+                0,
+                [79, 98],
+                "old * 19",
+                (23, 2, 3)
+            ),
+            Day11.monkey(
+                1,
+                [54, 65, 75, 74],
+                "old + 6",
+                (19, 2, 0)
+            ),
+            Day11.monkey(
+                2,
+                [79, 60, 97],
+                "old * old",
+                (13, 1, 3)
+            ),
+            Day11.monkey(
+                3,
+                [74],
+                "old + 3",
+                (17, 0, 1)
+            ),
+        ]
+        updated_monkey_items = [
+            [],
+            [54, 65, 75, 74],
+            [79, 60, 97],
+            [74, 500, 620],
+        ]
+        self.assertEqual(
+            updated_monkey_items,
+            [x.starting_items for x in Day11.monkey_turn(0,monkey_ls)]
+        )
+class TestRoundImplementer(unittest.TestCase):
+    def test_round_implementer(self):
+        monkey_ls = [
+            Day11.monkey(
+                0,
+                [79, 98],
+                "old * 19",
+                (23, 2, 3)
+            ),
+            Day11.monkey(
+                1,
+                [54, 65, 75, 74],
+                "old + 6",
+                (19, 2, 0)
+            ),
+            Day11.monkey(
+                2,
+                [79, 60, 97],
+                "old * old",
+                (13, 1, 3)
+            ),
+            Day11.monkey(
+                3,
+                [74],
+                "old + 3",
+                (17, 0, 1)
+            ),
+        ]
+        item_count_ls = [0, 0, 0, 0]
+        updated_monkey_items = [
+            [20, 23, 27, 26],
+            [2080, 25, 167, 207, 401, 1046],
+            [],
+            [],
+        ]
+        self.assertEqual(
+            updated_monkey_items,
+            [x.starting_items for x in Day11.round_implementer(monkey_ls, item_count_ls)[0]]
+        )
+
+class TestCalculateMonkeyBusiness(unittest.TestCase):
+    def test_calculate_monkey_business(self):
+        with open("Day11_test_input.txt") as input_file:
+            raw_input = input_file.read()
+        self.assertEqual(
+            10605,
+            Day11.calculate_monkey_business(raw_input, 20),
+        )
+
+
+if __name__ == '__main__':
+    unittest.main()
