3d38f8397b2a466fb2d968df3a3b6d1ed1ceb566
Your Name <you@example.com>
2023-02-02 13:28:47 -0800

    day 14 initialize, upadated for day 14 function names

--- Day14.py
+++ Day14.py
@@ -1,6 +1,7 @@
 from aocd import data
 
-
+def sand_counter(raw_data):
+    return sand_count
 
 if __name__ == '__main__':
     print(f"{data[4]}")
--- test_Day14.py
+++ test_Day14.py
@@ -3,12 +3,12 @@ import Day14
 
 
 class TestFindDecoderKey(unittest.TestCase):
-    def test_decoder_key(self):
-        with open("Day13_test_input.txt") as input_file:
+    def test_sand_counter(self):
+        with open("Day14_test_input.txt") as input_file:
             raw_data = input_file.read()
         self.assertEqual(
-            140,
-            Day13.find_decoder_key(raw_data)
+            24,
+            Day14.sand_counter(raw_data)
         )
 
 if __name__ == '__main__':
