a960ed219a0713d7dbfb3746bc505cdce4794f75
Your Name <you@example.com>
2023-02-03 23:34:35 -0800

    day14: passed part 1 and part 2 (with floor) passed test

--- Day14.py
+++ Day14.py
@@ -40,7 +40,7 @@ def diagram_rock_path(rock_path_ls, sand_source_coord):
                     rock_nodes_set.add((x_coord, y_coord))
     defined_nodes_dict["rock_nodes"] = rock_nodes_set
     # write sand source
-    defined_nodes_dict["origin node"] = set(sand_source_coord)
+    defined_nodes_dict["origin_node"] = set([sand_source_coord])
     defined_nodes_dict["sand_nodes"] = set()
     # what col value corresponds to index 0
     index_values_dict = {}
@@ -49,6 +49,31 @@ def diagram_rock_path(rock_path_ls, sand_source_coord):
     index_values_dict["max_y"] = max({value[1] for value in rock_nodes_set})
 
     return defined_nodes_dict, index_values_dict
+
+
+def visualize_cave(defined_nodes_dict):
+    """
+    prints the cave layout including all rock and sand cells
+    :param defined_nodes_dict: dictionary of defined rock and sand cells
+    :param index_values_dict: used to define sand origin point
+    :return:
+    """
+    node_set = defined_nodes_dict["rock_nodes"].union(defined_nodes_dict["sand_nodes"])
+
+    min_x = min({value[0] for value in node_set})
+    max_x = max({value[0] for value in node_set})
+    max_y = max({value[1] for value in node_set})
+    cave_map_ls = [["." for x in range(min_x, max_x + 1)] for y in
+                   range(max_y + 1)]
+    for node in defined_nodes_dict["rock_nodes"]:
+        cave_map_ls[node[1]][node[0] - min_x] = "#"
+    for node in defined_nodes_dict["sand_nodes"]:
+        cave_map_ls[node[1]][node[0] - min_x] = "o"
+    for node in defined_nodes_dict["origin_node"]:
+        cave_map_ls[node[1]][node[0] - min_x] = "+"
+
+    for row in cave_map_ls:
+        print("".join(row))
 
 
 def drop_sand(defined_nodes_dict, sand_coord, index_values_dict):
@@ -75,7 +100,7 @@ def drop_sand(defined_nodes_dict, sand_coord, index_values_dict):
             # try down and to the left
     drop_coord = (sand_coord[0] - 1, sand_coord[1] + 1)
     if (
-            (drop_coord[0] >= index_values_dict["max_x"]) or
+            (drop_coord[0] > index_values_dict["max_x"]) or
             (drop_coord[0] < index_values_dict["min_x"]) or
             (drop_coord[1] > index_values_dict["max_y"])
     ):  # off the grid, cave is full
@@ -86,7 +111,7 @@ def drop_sand(defined_nodes_dict, sand_coord, index_values_dict):
     else:  # hits rock or sand
         drop_coord = (sand_coord[0] + 1, sand_coord[1] + 1)  # try down and to the right
         if (
-                (drop_coord[0] >= index_values_dict["max_x"]) or
+                (drop_coord[0] > index_values_dict["max_x"]) or
                 (drop_coord[0] < index_values_dict["min_x"]) or
                 (drop_coord[1] > index_values_dict["max_y"])
         ):  # off the grid, cave is full
@@ -98,10 +123,10 @@ def drop_sand(defined_nodes_dict, sand_coord, index_values_dict):
             return sand_coord, False  # rests here
 
 
-def drop_sand_floor(sand_diagram_ls, sand_coord, index_values_dict):
+def drop_sand_floor(defined_nodes_dict, sand_coord, index_values_dict):
     """
     adds a single unit of sand and determines where it lands. assumes infinite floor
-    :param sand_diagram_ls: diagram of cave structure
+    :param defined_nodes_dict: diagram of cave structure
     :param sand_coord: coordinate where the sand starts from
     :param index_values_dict: value of the 0 point of the x array
     :return: final resting position of the unit of sand and whether the cave is full
@@ -111,43 +136,29 @@ def drop_sand_floor(sand_diagram_ls, sand_coord, index_values_dict):
 
     while down_space:  # drop down as far as possible
         drop_coord = (sand_coord[0], sand_coord[1] + 1)
-        if (
-                (drop_coord[0] - index_values_dict >= len(sand_diagram_ls[0])) or
-                (drop_coord[0] - index_values_dict < 0) or
-                (drop_coord[1] > len(sand_diagram_ls) - 1)
-        ):  # off the grid, cave is full
-            return (-1, -1), True
-        if sand_diagram_ls[drop_coord[1]][drop_coord[0] - index_values_dict] == '.':
+        if ((drop_coord in defined_nodes_dict["rock_nodes"].union(defined_nodes_dict["sand_nodes"])) or
+                (drop_coord[1] == index_values_dict["max_y"]+2)):  # hits rock or sand
+            down_space = False  # try diagonal directions
+        else:
             sand_coord = drop_coord
-        elif sand_diagram_ls[drop_coord[1]][drop_coord[0] - index_values_dict] in {'#', 'o'}:  # hits rock or sand
-            down_space = False
 
-    # try down and to the left
+            # try down and to the left
     drop_coord = (sand_coord[0] - 1, sand_coord[1] + 1)
-    if (
-            (drop_coord[0] - index_values_dict >= len(sand_diagram_ls[0])) or
-            (drop_coord[0] - index_values_dict < 0) or
-            (drop_coord[1] > len(sand_diagram_ls) - 1)
-    ):  # off the grid, cave is full
-        return (-1, -1), True
-    elif sand_diagram_ls[drop_coord[1]][drop_coord[0] - index_values_dict] == '.':  # drop down one spot
-        return drop_sand(sand_diagram_ls, drop_coord, index_values_dict)  # and remodel
-    elif sand_diagram_ls[drop_coord[1]][drop_coord[0] - index_values_dict] in {'#', 'o'}:  # hits rock or sand
+    if ((drop_coord not in defined_nodes_dict["rock_nodes"].union(
+            defined_nodes_dict["sand_nodes"])) and
+            (not drop_coord[1] == index_values_dict["max_y"]+2)):  # drop down one spot
+        return drop_sand_floor(defined_nodes_dict, drop_coord, index_values_dict)  # and remodel
+    else:  # hits rock or sand or floor
         drop_coord = (sand_coord[0] + 1, sand_coord[1] + 1)  # try down and to the right
-        if (
-                (drop_coord[0] - index_values_dict >= len(sand_diagram_ls[0])) or
-                (drop_coord[0] - index_values_dict < 0) or
-                (drop_coord[1] > len(sand_diagram_ls) - 1)
-        ):  # off the grid, cave is full
-            return (-1, -1), True
-        elif sand_diagram_ls[drop_coord[1]][drop_coord[0] - index_values_dict] == '.':  # drop down one spot
-            return drop_sand(sand_diagram_ls, drop_coord, index_values_dict)  # and remodel
-        elif sand_diagram_ls[drop_coord[1]][drop_coord[0] - index_values_dict] in {'#', 'o'}:  # hits rock or sand
-            # rests here
-            if sand_coord == (500, 0):
-                return sand_coord, True
+        if ((drop_coord not in defined_nodes_dict["rock_nodes"].union(
+                defined_nodes_dict["sand_nodes"])) and
+                (not drop_coord[1] == index_values_dict["max_y"]+2)):  # drop down one spot
+            return drop_sand_floor(defined_nodes_dict, drop_coord, index_values_dict)  # and remodel
+        else:  # hits rock or sand or floor
+            if sand_coord == next(iter(defined_nodes_dict["origin_node"])):
+                return (-1, -1), True
             else:
-                return sand_coord, False
+                return sand_coord, False  # rests here
 
 
 def add_sand(defined_nodes_dict, sand_source_coord, index_values_dict, has_floor):
@@ -163,12 +174,11 @@ def add_sand(defined_nodes_dict, sand_source_coord, index_values_dict, has_floor
     sand_count = 0
     full = False
 
-
     if has_floor == False:
 
         while not full:
             if sand_count == 1330:
-                print("foo")
+                visualize_cave(defined_nodes_dict)
             try:
                 rest_posn, full = drop_sand(defined_nodes_dict, sand_source_coord, index_values_dict)
             except IndexError:
@@ -180,13 +190,15 @@ def add_sand(defined_nodes_dict, sand_source_coord, index_values_dict, has_floor
 
     else:
         while not full:
+            if sand_count == 93:
+                visualize_cave(defined_nodes_dict)
             try:
                 rest_posn, full = drop_sand_floor(defined_nodes_dict, sand_source_coord, index_values_dict)
             except IndexError:
                 print(sand_count)
             if not full:
                 defined_nodes_dict["sand_nodes"].add(rest_posn)
-                sand_count += 1
+            sand_count += 1
         return sand_count
 
 
@@ -201,3 +213,4 @@ def sand_counter(raw_data, has_floor):
 
 if __name__ == '__main__':
     print(f"sand count = {sand_counter(data, False)}")
+    print(f"sand count = {sand_counter(data, True)}")
