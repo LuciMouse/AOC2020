24053f7e086892733fd4a5eba61f8223e57695ce
Your Name <you@example.com>
2023-02-16 22:56:36 -0800

    Day 17: make_rock_generator() is returning now returning fresh rock objects

--- Day17.py
+++ Day17.py
@@ -24,80 +24,79 @@ class FallingRock:
         self.coord_set = coord_set
 
 
+class RockGen:
+    """
+    generator function that returns rock objects
+    """
+
+    def __init__(self, name, coord_set):
+        self.name = name
+        self.coord_set = coord_set
+
+    def __iter__(self):
+        yield FallingRock(
+            self.name,
+            self.coord_set
+        )
+
+
 def make_rock_generator():
     """
     creates an infinite generator that returns the four rock objects
     :return: generator
     """
     # create the five rock types
-    rocks_ls = []
+    rock_type_ls = [
+        ('-',
+                {
+                    (0, 0),
+                    (1, 0),
+                    (2, 0),
+                    (3, 0),
+                }
+                ),
+        ('+',
 
-    rocks_ls.append(
-        lambda: FallingRock(
-            '-',
-            {
-                (0, 0),
-                (1, 0),
-                (2, 0),
-                (3, 0),
-            }
-        )
-    )
+                {
+                    (1, 0),
+                    (0, 1),
+                    (1, 1),
+                    (2, 1),
+                    (1, 2),
+                }
+                ),
+        ('L',
 
-    rocks_ls.append(
-        lambda: FallingRock(
-            '+',
+                {
+                    (0, 0),
+                    (1, 0),
+                    (2, 0),
+                    (2, 1),
+                    (2, 2),
+                }
+                ),
+        ('I',
 
-            {
-                (1, 0),
-                (0, 1),
-                (1, 1),
-                (2, 1),
-                (1, 2),
-            }
-        )
-    )
+                {
+                    (0, 0),
+                    (0, 1),
+                    (0, 2),
+                    (0, 3),
+                }
+                ),
+        ('.',
 
-    rocks_ls.append(
-        lambda: FallingRock(
-            'L',
-
-            {
-                (0, 0),
-                (1, 0),
-                (2, 0),
-                (2, 1),
-                (2, 2),
-            }
-        )
-    )
-
-    rocks_ls.append(
-        lambda: FallingRock(
-            'I',
-
-            {
-                (0, 0),
-                (0, 1),
-                (0, 2),
-                (0, 3),
-            }
-        )
-    )
-
-    rocks_ls.append(
-        FallingRock(
-            '.',
-
-            {
-                (0, 0),
-                (1, 0),
-                (0, 1),
-                (1, 1),
-            }
-        )
-    )
-    return itertools.cycle(rocks_ls)
+                {
+                    (0, 0),
+                    (1, 0),
+                    (0, 1),
+                    (1, 1),
+                }
+                ),
+    ]
+    rock_type_gen = itertools.cycle(rock_type_ls)
+    new_rock = (FallingRock(rock_type[0], rock_type[1]) for rock_type in rock_type_gen)
+    return new_rock
 
 
 def draw_chamber(curr_rock, top_layer):
@@ -117,7 +116,7 @@ def draw_chamber(curr_rock, top_layer):
 
     chamber_ls = ['-------']
 
-    for curr_y in range(highest_y+1):
+    for curr_y in range(highest_y + 1):
         y_ls = []
         for curr_x in range(7):
             if (curr_x, curr_y) in top_layer:
@@ -129,8 +128,10 @@ def draw_chamber(curr_rock, top_layer):
         chamber_ls.insert(0, ''.join(y_ls))
     return chamber_ls
 
+
 class UnknownPatternError(Exception):
     pass
+
 
 def model_falling_rocks(raw_input, num_rocks):
     """
@@ -148,7 +149,7 @@ def model_falling_rocks(raw_input, num_rocks):
 
     for i in range(num_rocks):
         curr_rock = next(rock_gen)
-        #postion drop point of the new rock
+        # postion drop point of the new rock
         x_offset = 2
         y_offset = top_point + 4
 
@@ -162,30 +163,30 @@ def model_falling_rocks(raw_input, num_rocks):
             if next_action == "jet":
                 next_jet_pattern = next(jet_pattern_gen)
                 if next_jet_pattern == ">":
-                    if max([x[0] for x in curr_rock.coord_set])<6: #there is room to move right
+                    if max([x[0] for x in curr_rock.coord_set]) < 6:  # there is room to move right
                         test_coord_set = {
                             (curr_coord[0] + 1, curr_coord[1]) for curr_coord in curr_rock.coord_set
                         }
                 elif next_jet_pattern == "<":
-                    if min([x[0] for x in curr_rock.coord_set])>0: #there is room to move left
+                    if min([x[0] for x in curr_rock.coord_set]) > 0:  # there is room to move left
                         test_coord_set = {
                             (curr_coord[0] - 1, curr_coord[1]) for curr_coord in curr_rock.coord_set
                         }
                 else:
                     raise UnknownPatternError("unknown jet pattern")
 
-                #does the proposed movement intersect with existing rock?
+                # does the proposed movement intersect with existing rock?
                 if not test_coord_set.intersection(top_layer):
-                    curr_rock.coord_set = test_coord_set #update coordinates
+                    curr_rock.coord_set = test_coord_set  # update coordinates
             elif next_action == 'fall':
                 test_coord_set = {
                     (curr_coord[0], curr_coord[1] - 1) for curr_coord in curr_rock.coord_set
                 }
-                #can it fall further?
-                if not test_coord_set.intersection(top_layer): #can fall further?
-                    curr_rock.coord_set = test_coord_set #update coordinates
-                else: #blocked
-                    #comes to rest
+                # can it fall further?
+                if not test_coord_set.intersection(top_layer):  # can fall further?
+                    curr_rock.coord_set = test_coord_set  # update coordinates
+                else:  # blocked
+                    # comes to rest
                     falling = False
                     top_point = max([x[1] for x in curr_rock.coord_set])
                     top_layer = top_layer.union(curr_rock.coord_set)
