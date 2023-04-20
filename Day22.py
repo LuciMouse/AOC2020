a10d6b2732199df14cd725c97bcce7ac3513b227
Your Name <you@example.com>
2023-04-20 14:53:55 -0700

    Day22: wrote parser to parse raw_data

--- Day22.py
+++ Day22.py
@@ -1,7 +1,22 @@
 from aocd import data
+import re
+
+
+def parse_input(raw_input):
+    split_data = raw_input.splitlines()
+    map_ls = split_data[:-2]
+    max_length = max(len(row) for row in map_ls)
+    map_ls = [row + " " * (max_length-len(row)) for row in map_ls]
+    split_directions = split_data[-1]
+    directions_ls = [(int(d), r) for d, r in re.findall("(\d+)(R|L)?", split_directions)]
+    return map_ls, directions_ls
+
 
 def monkey_map_1(raw_input):
+    map_ls, directions_ls = parse_input(raw_input)
+
     ...
+
 
 if __name__ == '__main__':
     print(f"{data[4]}")
--- test_Day22.py
+++ test_Day22.py
@@ -1,6 +1,19 @@
 import unittest
 import Day22
 
+class TestParseInput(unittest.TestCase):
+    def test_parse_input(self):
+        raw_input = "        ...#\n       .#..\n\n10R5L"
+        map_ls = [
+             "        ...#",
+            "       .#.. ",
+        ]
+        directions_ls = [(10, "R"), (5, "L")]
+        self.assertEqual(
+            (map_ls, directions_ls),
+            Day22.parse_input(raw_input)
+        )
+
 class TestMonkeyMap1(unittest.TestCase):
     def test_monkey_map_1(self):
         with open("Day22_test_input.txt","r") as input_file:
