7ddfa2d2472818e34ba6a4f46f8b88c0d63ec501
Your Name <you@example.com>
2023-04-21 16:30:59 -0700

    Day22: part 1 finished

--- Day22.py
+++ Day22.py
@@ -12,10 +12,28 @@ def parse_input(raw_input):
     return map_ls, directions_ls
 
 
-def inital_index(map_ls):
-    top_row = map_ls[0]
-    index = top_row.index(".")
+def first_open_tile_x(map_ls, row_index):
+    curr_row = map_ls[row_index]
+    index = [x == " " for x in curr_row].index(False)
     return index
+
+
+def last_open_tile_x(map_ls, row_index):
+    curr_row = map_ls[row_index][::-1]
+    index = [x == " " for x in curr_row].index(False)
+    return len(curr_row) - index - 1
+
+
+def first_open_tile_y(map_ls, col_index):
+    curr_col = [row[col_index] for row in map_ls]
+    index = [x == " " for x in curr_col].index(False)
+    return index
+
+
+def last_open_tile_y(map_ls, col_index):
+    curr_col = [row[col_index] for row in map_ls][::-1]
+    index = [x == " " for x in curr_col].index(False)
+    return len(curr_col) - index - 1
 
 
 def new_facing(curr_facing, turn_dir):
@@ -27,22 +45,112 @@ def new_facing(curr_facing, turn_dir):
     else:
         return result
 
+
+def parse_next_posn(path_ls, next_posn, next_posn_value):
+    if next_posn_value == "#":
+        return True, path_ls
+    elif next_posn_value == ".":
+        return False, path_ls + [next_posn]
+    else:
+        raise Exception("next position not defined")
+
+
+def take_step(map_ls, path_ls):
+    curr_posn, curr_facing = path_ls[-1]
+    if curr_facing == 0:  # right
+        new_x = curr_posn[1] + 1
+        if new_x == len(map_ls[0]):
+            new_x = first_open_tile_x(map_ls, curr_posn[0])
+        next_posn = (curr_posn[0], new_x)
+        next_posn_value = map_ls[next_posn[0]][next_posn[1]]
+        if next_posn_value == ' ':  # wrap off right
+            new_x = first_open_tile_x(map_ls, curr_posn[0])
+            next_posn = (curr_posn[0], new_x)
+            next_posn_value = map_ls[next_posn[0]][next_posn[1]]
+    elif curr_facing == 2:  # left
+        new_x = curr_posn[1] - 1
+        if new_x < 0:
+            new_x = last_open_tile_x(map_ls, curr_posn[0])
+        next_posn = (curr_posn[0], new_x)
+        next_posn_value = map_ls[next_posn[0]][next_posn[1]]
+        if next_posn_value == ' ':  # wrap off left
+            new_x = last_open_tile_x(map_ls, curr_posn[0])
+            next_posn = (curr_posn[0], new_x)
+            next_posn_value = map_ls[next_posn[0]][next_posn[1]]
+    elif curr_facing == 1:  # down
+        new_y = curr_posn[0] + 1
+        if new_y == len(map_ls):
+            new_y = first_open_tile_y(map_ls, curr_posn[1])
+        next_posn = (new_y, curr_posn[1])
+        next_posn_value = map_ls[next_posn[0]][next_posn[1]]
+        if next_posn_value == ' ':  # wrap off bottom
+            new_y = first_open_tile_y(map_ls, curr_posn[1])
+            next_posn = (new_y, curr_posn[1])
+            next_posn_value = map_ls[next_posn[0]][next_posn[1]]
+    elif curr_facing == 3:  # up
+        new_y = curr_posn[0] - 1
+        if new_y < 0:
+            new_y = last_open_tile_y(map_ls, curr_posn[1])
+        next_posn = (new_y, curr_posn[1])
+        next_posn_value = map_ls[next_posn[0]][next_posn[1]]
+        if next_posn_value == ' ':  # wrap off bottom
+            new_y = last_open_tile_y(map_ls, curr_posn[1])
+            next_posn = (new_y, curr_posn[1])
+            next_posn_value = map_ls[next_posn[0]][next_posn[1]]
+
+    return parse_next_posn(path_ls, (next_posn, curr_facing), next_posn_value)
+
+
 def implement_instruction(path_ls, map_ls, instruction):
-    curr_posn = path_ls[-1][0]
     num_steps = instruction[0]
+    blocked = False
+    # move
+    while (num_steps > 0) & (not blocked):
+        blocked, path_ls = take_step(
+            map_ls,
+            path_ls
+        )
+        num_steps -= 1
+    # rotate
+    last_step = path_ls[-1]
+    path_ls[-1] = (last_step[0], new_facing(last_step[1], instruction[1]))
+
+    return path_ls
+
+
+def print_path(map_ls, path_ls):
+    working_map_ls = [[*row] for row in map_ls]
+    facing_dict = {0: ">", 1: "V", 2: "<", 3: "^"}
+    for node in path_ls:
+        working_map_ls[node[0][0]][node[0][1]] = facing_dict[node[1]]
+    working_map_ls = ["".join(row) for row in working_map_ls]
+    for row in working_map_ls:
+        print(row)
+    print("\n\n")
+    return working_map_ls
+
+
+def calculate_password(position):
+    row = position[0][0] + 1
+    col = position[0][1] + 1
+    facing = position[1]
+
+    return (1000 * row) + (4 * col) + facing
 
 
 def monkey_map_1(raw_input):
     map_ls, directions_ls = parse_input(raw_input)
     facing = 0
-    curr_index = inital_index(map_ls)
+    curr_index = first_open_tile_x(map_ls, 0)
     curr_posn = (0, curr_index)
     path_ls = [(curr_posn, facing)]
 
     for curr_instruction in directions_ls:
-        implement_instruction(path_ls, map_ls, curr_instruction)
-    ...
+        path_ls = implement_instruction(path_ls, map_ls, curr_instruction)
+        #print_path(map_ls, path_ls)
+    password = calculate_password(path_ls[-1])
+    return password
 
 
 if __name__ == '__main__':
-    print(f"{data[4]}")
+    print(f"part1:{monkey_map_1(data)}")
--- test_Day22.py
+++ test_Day22.py
@@ -1,11 +1,12 @@
 import unittest
 import Day22
 
+
 class TestParseInput(unittest.TestCase):
     def test_parse_input(self):
         raw_input = "        ...#\n       .#..\n\n10R5L5"
         map_ls = [
-             "        ...#",
+            "        ...#",
             "       .#.. ",
         ]
         directions_ls = [(10, 1), (5, -1), (5, 0)]
@@ -13,58 +14,693 @@ class TestParseInput(unittest.TestCase):
             (map_ls, directions_ls),
             Day22.parse_input(raw_input)
         )
-class TestInitialIndex(unittest.TestCase):
-    def test_initail_index_1(self):
+
+
+class TestFirstOpenTileX(unittest.TestCase):
+    def test_first_open_tile_x_1(self):
         map_ls = [
-             "        ...#",
+            "        ...#",
             "       .#.. ",
         ]
         self.assertEqual(
             8,
-            Day22.inital_index(map_ls)
+            Day22.first_open_tile_x(map_ls, 0)
         )
+    def test_first_open_tile_x_2(self):
+        map_ls = [
+            "        ...#",
+            "        #.. ",
+        ]
+        self.assertEqual(
+            8,
+            Day22.first_open_tile_x(map_ls, 1)
+        )
+
+
+class TestLastOpenTileX(unittest.TestCase):
+    def test_last_open_tile_x_1(self):
+        map_ls = [
+            "        ...#",
+            "       .#.. ",
+        ]
+        self.assertEqual(
+            10,
+            Day22.last_open_tile_x(map_ls, 1)
+        )
+    def test_last_open_tile_x_2(self):
+        map_ls = [
+            "        ...#",
+            "       .#.. ",
+        ]
+        self.assertEqual(
+            11,
+            Day22.last_open_tile_x(map_ls, 0)
+        )
+
+
+class TestFirstOpenTileY(unittest.TestCase):
+    def test_first_open_tile_y_1(self):
+        map_ls = [
+            "        ...#",
+            "       .#.. ",
+        ]
+        self.assertEqual(
+            1,
+            Day22.first_open_tile_y(map_ls, 7)
+        )
+
+    def test_first_open_tile_y_2(self):
+        map_ls = [
+            "        ...#",
+            "       .#.. ",
+        ]
+        self.assertEqual(
+            0,
+            Day22.first_open_tile_y(map_ls, 9)
+        )
+
+    def test_first_open_tile_y_3(self):
+        map_ls = [
+            "        ...#",
+            "       .#...",
+        ]
+        self.assertEqual(
+            0,
+            Day22.first_open_tile_y(map_ls, 10)
+        )
+
+
+class TestLastOpenTileY(unittest.TestCase):
+    def test_last_open_tile_y_1(self):
+        map_ls = [
+            "        ...#",
+            "      .#..  ",
+        ]
+        self.assertEqual(
+            0,
+            Day22.last_open_tile_y(map_ls, 10)
+        )
+
+    def test_last_open_tile_y_2(self):
+        map_ls = [
+            "        ...#",
+            "      .#..  ",
+        ]
+        self.assertEqual(
+            1,
+            Day22.last_open_tile_y(map_ls, 9)
+        )
+
+    def test_last_open_tile_y_3(self):
+        map_ls = [
+            "       ....#",
+            "      .#..  ",
+        ]
+        self.assertEqual(
+            1,
+            Day22.last_open_tile_y(map_ls, 7)
+        )
+
+
 class TestNewFacing(unittest.TestCase):
     def test_new_facing_R(self):
         self.assertEqual(
             1,
             Day22.new_facing(0, 1)
         )
+
     def test_new_facing_L(self):
         self.assertEqual(
             2,
             Day22.new_facing(3, -1)
         )
+
     def test_new_facing_R_wrap(self):
         self.assertEqual(
             0,
             Day22.new_facing(3, 1)
         )
+
     def test_new_facing_L_wrap(self):
         self.assertEqual(
             3,
             Day22.new_facing(0, -1)
         )
-class TestImplementInstruction(unittest.TestCase):
-    def test_implement_instruction_simple_horiz(self):
+
+    def test_new_facing_0(self):
+        self.assertEqual(
+            3,
+            Day22.new_facing(3, 0)
+        )
+
+
+class TestTakeStep(unittest.TestCase):
+    def test_take_step_simple_horiz_R(self):
         map_ls = [
             "        ...#",
             "       .#.. ",
         ]
         path_ls = [((0, 8), 0)]
-        instruction = "2R"
 
-        Day22.implement_instruction(
-            path_ls,
-            map_ls,
-            instruction
+        self.assertEqual(
+            (False, [((0, 8), 0), ((0, 9), 0)]),
+            Day22.take_step(map_ls, path_ls)
         )
+
+    def test_take_step_simple_horiz_L(self):
+        map_ls = [
+            "        ...#",
+            "       .#.. ",
+        ]
+        path_ls = [((0, 9), 2)]
+
+        self.assertEqual(
+            (False, [((0, 9), 2), ((0, 8), 2)]),
+            Day22.take_step(map_ls, path_ls)
+        )
+
+    def test_take_step_horiz_R_block(self):
+        map_ls = [
+            "        ...#",
+            "       .#.. ",
+        ]
+        path_ls = [((1, 7), 0)]
+
+        self.assertEqual(
+            (True, [((1, 7), 0)]),
+            Day22.take_step(map_ls, path_ls)
+        )
+
+    def test_take_step_horiz_L_block(self):
+        map_ls = [
+            "        ...#",
+            "       .#.. ",
+        ]
+        path_ls = [((1, 9), 2)]
+
+        self.assertEqual(
+            (True, [((1, 9), 2)]),
+            Day22.take_step(map_ls, path_ls)
+        )
+
+    def test_take_step_horiz_R_index_wrap(self):
+        map_ls = [
+            "        ...#",
+            "       ..#..",
+        ]
+        path_ls = [((1, 11), 0)]
+
+        self.assertEqual(
+            (False, [((1, 11), 0), ((1, 7), 0)]),
+            Day22.take_step(map_ls, path_ls)
+        )
+
+    def test_take_step_horiz_L_index_wrap(self):
+        map_ls = [
+            "        ...#",
+            ".#..        ",
+        ]
+        path_ls = [((1, 0), 2)]
+
+        self.assertEqual(
+            (False, [((1, 0), 2), ((1, 3), 2)]),
+            Day22.take_step(map_ls, path_ls)
+        )
+
+    def test_take_step_horiz_R_space_wrap(self):
+        map_ls = [
+            "        ...#",
+            "       .#.. ",
+        ]
+        path_ls = [((1, 10), 0)]
+
+        self.assertEqual(
+            (False, [((1, 10), 0), ((1, 7), 0)]),
+            Day22.take_step(map_ls, path_ls)
+        )
+
+    def test_take_step_horiz_L_space_wrap(self):
+        map_ls = [
+            "        ...#",
+            "       .#.. ",
+        ]
+        path_ls = [((1, 7), 2)]
+
+        self.assertEqual(
+            (False, [((1, 7), 2), ((1, 10), 2)]),
+            Day22.take_step(map_ls, path_ls)
+        )
+
+    def test_take_step_simple_vert_U(self):
+        map_ls = [
+            "        ...#",
+            "       .#.. ",
+        ]
+        path_ls = [((1, 9), 3)]
+
+        self.assertEqual(
+            (False, [((1, 9), 3), ((0, 9), 3)]),
+            Day22.take_step(map_ls, path_ls)
+        )
+
+    def test_take_step_simple_vert_D(self):
+        map_ls = [
+            "        ...#",
+            "       .#.. ",
+        ]
+        path_ls = [((0, 9), 1)]
+
+        self.assertEqual(
+            (False, [((0, 9), 1), ((1, 9), 1)]),
+            Day22.take_step(map_ls, path_ls)
+        )
+
+    def test_take_step_vert_U_block(self):
+        map_ls = [
+            "        ...#",
+            "       .#...",
+        ]
+        path_ls = [((1, 11), 3)]
+
+        self.assertEqual(
+            (True, [((1, 11), 3)]),
+            Day22.take_step(map_ls, path_ls)
+        )
+
+    def test_take_step_vert_D_block(self):
+        map_ls = [
+            "        ...#",
+            "       .#.. ",
+        ]
+        path_ls = [((0, 8), 1)]
+
+        self.assertEqual(
+            (True, [((0, 8), 1)]),
+            Day22.take_step(map_ls, path_ls)
+        )
+
+    def test_take_step_vert_U_index_wrap(self):
+        map_ls = [
+            "        ...#",
+            "       ..#. ",
+        ]
+        path_ls = [((0, 10), 3)]
+
+        self.assertEqual(
+            (False, [((0, 10), 3), ((1, 10), 3)]),
+            Day22.take_step(map_ls, path_ls)
+        )
+
+    def test_take_step_vert_D_index_wrap(self):
+        map_ls = [
+            "        ...#",
+            "       ..#. ",
+        ]
+        path_ls = [((1, 10), 1)]
+
+        self.assertEqual(
+            (False, [((1, 10), 1), ((0, 10), 1)]),
+            Day22.take_step(map_ls, path_ls)
+        )
+
+    def test_take_step_vert_U_space_wrap(self):
+        map_ls = [
+            "        ...#",
+            "       .#.. ",
+            "       .#.. ",
+        ]
+        path_ls = [((1, 7), 3)]
+
+        self.assertEqual(
+            (False, [((1, 7), 3), ((2, 7), 3)]),
+            Day22.take_step(map_ls, path_ls)
+        )
+
+    def test_take_step_vert_D_space_wrap(self):
+        map_ls = [
+            "        ...#",
+            "       .#.. ",
+            "       .#.  ",
+        ]
+        path_ls = [((1, 10), 1)]
+
+        self.assertEqual(
+            (False, [((1, 10), 1), ((0, 10), 1)]),
+            Day22.take_step(map_ls, path_ls)
+        )
+
+
+class TestImplementInstruction(unittest.TestCase):
+    def test_implement_instruction_simple_horiz_R(self):
+        map_ls = [
+            "        ...#",
+            "       .#.. ",
+        ]
+        path_ls = [((0, 8), 0)]
+        instruction = (2, 1)
+
         self.assertEqual(
             [((0, 8), 0), ((0, 9), 0), ((0, 10), 1)],
-            path_ls
+            Day22.implement_instruction(
+                path_ls,
+                map_ls,
+                instruction
+            )
         )
+
+    def test_implement_instruction_simple_horiz_L(self):
+        map_ls = [
+            "        ...#",
+            "       .#.. ",
+        ]
+        path_ls = [((0, 10), 2)]
+        instruction = (2, -1)
+
+        self.assertEqual(
+            [((0, 10), 2), ((0, 9), 2), ((0, 8), 1)],
+            Day22.implement_instruction(
+                path_ls,
+                map_ls,
+                instruction
+            )
+        )
+
+    def test_implement_instruction_simple_vert_U(self):
+        map_ls = [
+            "        ...#",
+            "       .#.. ",
+            "       .#.. ",
+        ]
+        path_ls = [((2, 10), 3)]
+        instruction = (2, -1)
+
+        self.assertEqual(
+            [((2, 10), 3), ((1, 10), 3), ((0, 10), 2)],
+            Day22.implement_instruction(
+                path_ls,
+                map_ls,
+                instruction
+            )
+        )
+
+    def test_implement_instruction_simple_vert_D(self):
+        map_ls = [
+            "        ...#",
+            "       .#.. ",
+            "       .#.. ",
+        ]
+        path_ls = [((0, 10), 1)]
+        instruction = (2, 1)
+
+        self.assertEqual(
+            [((0, 10), 1), ((1, 10), 1), ((2, 10), 2)],
+            Day22.implement_instruction(
+                path_ls,
+                map_ls,
+                instruction
+            )
+        )
+
+    def test_implement_instruction_block_horiz_R(self):
+        map_ls = [
+            "        ...#",
+            "       .#.. ",
+        ]
+        path_ls = [((0, 9), 0)]
+        instruction = (2, -1)
+
+        self.assertEqual(
+            [((0, 9), 0), ((0, 10), 3)],
+            Day22.implement_instruction(
+                path_ls,
+                map_ls,
+                instruction
+            )
+        )
+
+    def test_implement_instruction_block_horiz_L(self):
+        map_ls = [
+            "        ...#",
+            "      .#... ",
+        ]
+        path_ls = [((1, 10), 2)]
+        instruction = (3, -1)
+
+        self.assertEqual(
+            [((1, 10), 2), ((1, 9), 2), ((1, 8), 1)],
+            Day22.implement_instruction(
+                path_ls,
+                map_ls,
+                instruction
+            )
+        )
+
+    def test_implement_instruction_block_vert_U(self):
+        map_ls = [
+            "        ..#.",
+            "       .#.. ",
+            "       .#.. ",
+        ]
+        path_ls = [((2, 10), 3)]
+        instruction = (2, 1)
+
+        self.assertEqual(
+            [((2, 10), 3), ((1, 10), 0)],
+            Day22.implement_instruction(
+                path_ls,
+                map_ls,
+                instruction
+            )
+        )
+
+    def test_implement_instruction_block_vert_D(self):
+        map_ls = [
+            "        ...#",
+            "       .#.. ",
+            "       .#.. ",
+        ]
+        path_ls = [((0, 8), 1)]
+        instruction = (2, 1)
+
+        self.assertEqual(
+            [((0, 8), 2)],
+            Day22.implement_instruction(
+                path_ls,
+                map_ls,
+                instruction
+            )
+        )
+
+    def test_implement_instruction_index_wrap_horiz_R(self):
+        map_ls = [
+            "        ...#",
+            "       .#...",
+        ]
+        path_ls = [((1, 9), 0)]
+        instruction = (3, -1)
+
+        self.assertEqual(
+            [((1, 9), 0), ((1, 10), 0), ((1, 11), 0), ((1, 7), 3)],
+            Day22.implement_instruction(
+                path_ls,
+                map_ls,
+                instruction
+            )
+        )
+
+    def test_implement_instruction_index_wrap_horiz_L(self):
+        map_ls = [
+            "        ...#",
+            "..#...      ",
+        ]
+        path_ls = [((1, 1), 2)]
+        instruction = (3, -1)
+
+        self.assertEqual(
+            [((1, 1), 2), ((1, 0), 2), ((1, 5), 2), ((1, 4), 1)],
+            Day22.implement_instruction(
+                path_ls,
+                map_ls,
+                instruction
+            )
+        )
+
+    def test_implement_instruction_index_wrap_vert_U(self):
+        map_ls = [
+            "        ..#.",
+            "       .#.. ",
+            "       .#.. ",
+        ]
+        path_ls = [((1, 9), 3)]
+        instruction = (2, 1)
+
+        self.assertEqual(
+            [((1, 9), 3), ((0, 9), 3), ((2, 9), 0)],
+            Day22.implement_instruction(
+                path_ls,
+                map_ls,
+                instruction
+            )
+        )
+
+    def test_implement_instruction_index_wrap_vert_D(self):
+        map_ls = [
+            "        ...#",
+            "       .#.. ",
+            "       .#.. ",
+        ]
+        path_ls = [((0, 9), 1)]
+        instruction = (3, 1)
+
+        self.assertEqual(
+            [((0, 9), 1), ((1, 9), 1), ((2, 9), 1), ((0, 9), 2)],
+            Day22.implement_instruction(
+                path_ls,
+                map_ls,
+                instruction
+            )
+        )
+
+    def test_implement_instruction_space_wrap_horiz_R(self):
+        map_ls = [
+            "        ...#",
+            "     .#.... ",
+        ]
+        path_ls = [((1, 9), 0)]
+        instruction = (3, -1)
+
+        self.assertEqual(
+            [((1, 9), 0), ((1, 10), 0), ((1, 5), 3)],
+            Day22.implement_instruction(
+                path_ls,
+                map_ls,
+                instruction
+            )
+        )
+
+    def test_implement_instruction_space_wrap_horiz_L(self):
+        map_ls = [
+            "        ...#",
+            "      .#... ",
+        ]
+        path_ls = [((1, 6), 2)]
+        instruction = (3, -1)
+
+        self.assertEqual(
+            [((1, 6), 2), ((1, 10), 2), ((1, 9), 2), ((1, 8), 1)],
+            Day22.implement_instruction(
+                path_ls,
+                map_ls,
+                instruction
+            )
+        )
+
+    def test_implement_instruction_space_wrap_vert_U(self):
+        map_ls = [
+            "        ..#.",
+            "       .#.. ",
+            "       .#.. ",
+            "       .#.. ",
+        ]
+        path_ls = [((1, 7), 3)]
+        instruction = (2, 1)
+
+        self.assertEqual(
+            [((1, 7), 3), ((3, 7), 3), ((2, 7), 0)],
+            Day22.implement_instruction(
+                path_ls,
+                map_ls,
+                instruction
+            )
+        )
+
+    def test_implement_instruction_space_wrap_vert_D(self):
+        map_ls = [
+            "        ...#",
+            "       .#.. ",
+            "     .#..   ",
+        ]
+        path_ls = [((0, 9), 1)]
+        instruction = (2, 1)
+
+        self.assertEqual(
+            [((0, 9), 1), ((1, 9), 1), ((0, 9), 2)],
+            Day22.implement_instruction(
+                path_ls,
+                map_ls,
+                instruction
+            )
+        )
+
+    def test_implement_instruction_1(self):
+        map_ls = [
+            '        ...#    ',
+            '        .#..    ',
+            '        #...    ',
+            '        ....    ',
+            '...#.......#    ',
+            '........#...    ',
+            '..#....#....    ',
+            '..........#.    ',
+            '        ...#....',
+            '        .....#..',
+            '        .#......',
+            '        ......#.'
+        ]
+        path_ls = [((5, 3), 1)]
+        instruction = (10, -1)
+
+        self.assertEqual(
+            [((5, 3), 1), ((6, 3), 1), ((7, 3), 0)],
+            Day22.implement_instruction(
+                path_ls,
+                map_ls,
+                instruction
+            )
+        )
+
+
+class TestPrintPath(unittest.TestCase):
+    def test_print_path(self):
+        map_ls = [
+            "        ..#.",
+            "       .#.. ",
+            "       .#.. ",
+            "       .#.. ",
+        ]
+        path_ls = [
+            ((0, 8), 0), ((0, 9), 1),
+            ((1, 9), 1), ((2, 9), 1), ((3, 9), 0),
+            ((3, 10), 0), ((3, 7), 3),
+            ((2, 7), 3)
+        ]
+        path_diagram_ls = [
+            "        >V#.",
+            "       .#V. ",
+            "       ^#V. ",
+            "       ^#>> ",
+        ]
+        self.assertEqual(
+            path_diagram_ls,
+            Day22.print_path(
+                map_ls,
+                path_ls
+            )
+        )
+
+
+class TestCalculatePassword(unittest.TestCase):
+    def test_calculate_password(self):
+        postion = ((5, 7), 0)
+        self.assertEqual(
+            6032,
+            Day22.calculate_password(postion)
+        )
+
+
 class TestMonkeyMap1(unittest.TestCase):
     def test_monkey_map_1(self):
-        with open("Day22_test_input.txt","r") as input_file:
+        with open("Day22_test_input.txt", "r") as input_file:
             raw_input = input_file.read()
         self.assertEqual(
             6032,
