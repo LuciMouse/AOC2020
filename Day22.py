04a7761ae342e370a5b128604e012ca61db70f40
Your Name <you@example.com>
2023-04-21 12:34:21 -0700

    Day22: functions to determine inital x_posn (inital_index() ) and facing direction

--- Day22.py
+++ Day22.py
@@ -6,15 +6,41 @@ def parse_input(raw_input):
     split_data = raw_input.splitlines()
     map_ls = split_data[:-2]
     max_length = max(len(row) for row in map_ls)
-    map_ls = [row + " " * (max_length-len(row)) for row in map_ls]
+    map_ls = [row + " " * (max_length - len(row)) for row in map_ls]
     split_directions = split_data[-1]
-    directions_ls = [(int(d), r) for d, r in re.findall("(\d+)(R|L)?", split_directions)]
+    directions_ls = [(int(d), dict(R=1, L=-1).get(r, 0)) for d, r in re.findall("(\d+)(R|L)?", split_directions)]
     return map_ls, directions_ls
+
+
+def inital_index(map_ls):
+    top_row = map_ls[0]
+    index = top_row.index(".")
+    return index
+
+
+def new_facing(curr_facing, turn_dir):
+    result = curr_facing + turn_dir
+    if result < 0:
+        return 3
+    elif result > 3:
+        return 0
+    else:
+        return result
+
+def implement_instruction(path_ls, map_ls, instruction):
+    curr_posn = path_ls[-1][0]
+    num_steps = instruction[0]
 
 
 def monkey_map_1(raw_input):
     map_ls, directions_ls = parse_input(raw_input)
+    facing = 0
+    curr_index = inital_index(map_ls)
+    curr_posn = (0, curr_index)
+    path_ls = [(curr_posn, facing)]
 
+    for curr_instruction in directions_ls:
+        implement_instruction(path_ls, map_ls, curr_instruction)
     ...
 
 
--- test_Day22.py
+++ test_Day22.py
@@ -3,17 +3,65 @@ import Day22
 
 class TestParseInput(unittest.TestCase):
     def test_parse_input(self):
-        raw_input = "        ...#\n       .#..\n\n10R5L"
+        raw_input = "        ...#\n       .#..\n\n10R5L5"
         map_ls = [
              "        ...#",
             "       .#.. ",
         ]
-        directions_ls = [(10, "R"), (5, "L")]
+        directions_ls = [(10, 1), (5, -1), (5, 0)]
         self.assertEqual(
             (map_ls, directions_ls),
             Day22.parse_input(raw_input)
         )
+class TestInitialIndex(unittest.TestCase):
+    def test_initail_index_1(self):
+        map_ls = [
+             "        ...#",
+            "       .#.. ",
+        ]
+        self.assertEqual(
+            8,
+            Day22.inital_index(map_ls)
+        )
+class TestNewFacing(unittest.TestCase):
+    def test_new_facing_R(self):
+        self.assertEqual(
+            1,
+            Day22.new_facing(0, 1)
+        )
+    def test_new_facing_L(self):
+        self.assertEqual(
+            2,
+            Day22.new_facing(3, -1)
+        )
+    def test_new_facing_R_wrap(self):
+        self.assertEqual(
+            0,
+            Day22.new_facing(3, 1)
+        )
+    def test_new_facing_L_wrap(self):
+        self.assertEqual(
+            3,
+            Day22.new_facing(0, -1)
+        )
+class TestImplementInstruction(unittest.TestCase):
+    def test_implement_instruction_simple_horiz(self):
+        map_ls = [
+            "        ...#",
+            "       .#.. ",
+        ]
+        path_ls = [((0, 8), 0)]
+        instruction = "2R"
 
+        Day22.implement_instruction(
+            path_ls,
+            map_ls,
+            instruction
+        )
+        self.assertEqual(
+            [((0, 8), 0), ((0, 9), 0), ((0, 10), 1)],
+            path_ls
+        )
 class TestMonkeyMap1(unittest.TestCase):
     def test_monkey_map_1(self):
         with open("Day22_test_input.txt","r") as input_file:
